41bba1b328f89becdec85cce64255cbc3d5e9cf4
escapeboy <katsarov@gmail.com>
2014-04-13 17:04:30 +0300

    clearing some data

--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,9 @@
 <?php
 return array(
-		'api_key' => 'jCLEDcLge8RAb5ICWxSVRUeBEG8FGbfYHzdweiyleKZbI2qNeAGgKy5CoaNBMekO',
-		'api_secret' => 'P8sRiPyFOAmXiDsvrXfmrl6McttRTkmAMGtzeMp6udRv03N8w4r7GG7RhYIselIC',
+		'api_key' => '',
+		'api_secret' => '',
 		'api_version' => '3.0',
 		'cache_time' => 60,
-		'forum'	=> 'katsarov'
+		'forum'	=> ''
 	);
 ?>

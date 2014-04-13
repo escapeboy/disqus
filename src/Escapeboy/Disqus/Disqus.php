c919fae97b70889a8881e274309eee1ebefd6ec4
escapeboy <katsarov@gmail.com>
2014-04-13 22:41:54 +0300

    added post functionality

--- src/Escapeboy/Disqus/Disqus.php
+++ src/Escapeboy/Disqus/Disqus.php
@@ -2,6 +2,14 @@
  
 class Disqus {
 
+	/**
+	*
+	* Get data from Disqus API
+	* (string) $section required
+	* (string) $method required
+	* (array) $params
+	*
+	**/
 
 	public function get($section, $method, $params=array()){
 
@@ -28,17 +36,49 @@ class Disqus {
 	  	return $result;
 	}
 
+	/**
+	*
+	* Send data to Disqus API
+	* (string) $section required
+	* (string) $method required
+	* (array) $params
+	*
+	**/
+	public function post($section, $method, $params=array()){
+
+		foreach ($params as $key => $value) {
+			$vars.=$key.'='.$value.'&';
+		}
+		$endpoint = 'http://disqus.com/api/'.\Config::get('disqus::api_version').'/'.$section.'/'.$method.'.json?api_key='.urlencode(\Config::get('disqus::api_key'));
+
+		$session = curl_init($endpoint);
+		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($session, CURLOPT_POST, count($params));
+		curl_setopt($session, CURLOPT_POSTFIELDS, $vars);
+		
+		$result = curl_exec($session);
+		curl_close($session);
+
+		return $result;
+	}
+
+
+	/**
+	 * Return total comments in thread
+	 * (string) $thread_url required
+	 */
 	public function commentsCount($thread_url)
 	{
 		$thread = self::getThread($thread_url);
 		return $thread->response->posts;
 	}
 
+	/**
+	 * Return thread details
+	 * (string) $thread_url required
+	 */
 	public function getThread($thread_url)
 	{
 		return json_decode($this->get('threads', 'details', array('thread:link' => $thread_url)));
 	}
-
-
- 
 }
